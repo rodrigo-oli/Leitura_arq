1340b550780d24b15855eef39585c62541229074
rodrigo-oli <42010810+rodrigo-oli@users.noreply.github.com>
2023-02-17 17:01:40 -0300

    Update Leitura_arq.py

--- Leitura_arq.py
+++ Leitura_arq.py
@@ -8,13 +8,13 @@ def analisar_dados(arquivo):
     
     # Verifica a quantidade de valores duplicados nas colunas item e model
     qtd_duplicados = df[['model_id']].duplicated().sum()
-    st.write(f'{qtd_duplicados}, Valores duplicados na coluna model')
+    st.write(f'{qtd_duplicados}, Valores duplicados na coluna model_id')
        
     # Verifica se a coluna "promo" contém apenas números inteiros
     if pd.api.types.is_integer_dtype(df["promo_stock"]):
-        st.write("Todos os valores da coluna 'promo_stock' são inteiros.")
+        #st.write("Todos os valores da coluna 'promo_stock' são inteiros.")
     else:
-        st.write("A coluna 'promo_stock' contém valores não inteiros.")
+        st.write("A coluna 'promo_stock' contém valores NÃO inteiros(ex:2,5)")
     
     # Verifica se as colunas "start_date" e "end_date" estão no formato correto
     date_cols = ["start_date", "end_date"]
@@ -26,11 +26,11 @@ def analisar_dados(arquivo):
         if n_incorrect > 0:
             st.write(f"{n_incorrect} valores na coluna {col} não está no formato correto.")
     if n_dates_incorrect == 0:
-        st.write("Todas as datas estão no formato correto.")
+        #st.write("Todas as datas estão no formato correto.")
     
         # Verifica se a coluna "preco_negociado" contém valores menores ou iguais a zero
     if (df["preco_negociado"] == 0).any():
-        st.write("A coluna 'preco_negociado' contém valores iguais ou menores que zero!!.")
+        st.write("A coluna 'preco_negociado' contém valores = zero!!.")
    
 
 
