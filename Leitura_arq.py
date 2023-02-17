63e7edb07eca7cc4b48ea179a1678facf11a60a3
rodrigo-oli <42010810+rodrigo-oli@users.noreply.github.com>
2023-02-17 17:13:02 -0300

    Update Leitura_arq.py

--- Leitura_arq.py
+++ Leitura_arq.py
@@ -14,7 +14,7 @@ def analisar_dados(arquivo):
     if pd.api.types.is_integer_dtype(df["promo_stock"]):
         st.write("promo_stock ok.")
     else:
-        st.write("A coluna 'promo_stock' contém valores NÃO inteiros(ex:2,5)")
+        st.write("Coluna 'promo_stock' contém valores NÃO inteiros (ex:2,5)")
     
     # Verifica se as colunas "start_date" e "end_date" estão no formato correto
     date_cols = ["start_date", "end_date"]
@@ -26,11 +26,11 @@ def analisar_dados(arquivo):
         if n_incorrect > 0:
             st.write(f"{n_incorrect} valores na coluna {col} não está no formato correto.")
     if n_dates_incorrect == 0:
-        st.write("Datas ok.")
+        st.write("Data ok.")
     
         # Verifica se a coluna "preco_negociado" contém valores menores ou iguais a zero
     if (df["preco_negociado"] == 0).any():
-        st.write("A coluna 'preco_negociado' contém valores = zero!!.")
+        st.write("Coluna 'preco_negociado' contém valores = zero!!")
    
 
 
